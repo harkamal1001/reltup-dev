922cf660ddf840efd9567514a4edda882670ae77
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-25 11:33:52 +0530

    conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://reltup-dev-wbc.herokuapp.com/',
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
