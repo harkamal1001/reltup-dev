a3f812bfe6583012b54fd2a242da55f50c09e20d
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 15:04:21 +0530

    server

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ var mongoose   = require('mongoose');
 var config 	   = require('./config');
 var path 	   = require('path');
 
+// APP CONFIGURATION ==================
+// ====================================
+// use body parser so we can grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
