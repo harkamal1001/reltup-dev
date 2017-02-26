cc7ea281a0b950ee61adc37c06aa85323f4fe390
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 15:12:23 +0530

    conf

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://hradmin:hradmin@cluster0-shard-00-00-qvxmx.mongodb.net:27017,cluster0-shard-00-01-qvxmx.mongodb.net:27017,cluster0-shard-00-02-qvxmx.mongodb.net:27017/shopDb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+	'database': 'mongodb://hradmin:hradmin@cluster0-shard-00-00-qvxmx.mongodb.net:27017,cluster0-shard-00-01-qvxmx.mongodb.net:27017,cluster0-shard-00-02-qvxmx.mongodb.net:27017/reltup?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
