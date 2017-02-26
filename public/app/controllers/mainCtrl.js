71422f4f2f201f680fe570bfe50bb27ba7cff18f
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 11:19:18 +0530

    email pattern

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -3,4 +3,8 @@ angular.module('mainCtrl', [])
 .controller('mainController', function() {
 	var vm = this;	
 	vm.message = 'this is my message!';
+    
+    
+    vm.username = '\w+@illinois.edu';
+
 });
