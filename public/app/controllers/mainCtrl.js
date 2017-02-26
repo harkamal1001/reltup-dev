325dd99f986ee94c1a180a0f9a2ee2a9ad023ebf
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 11:52:43 +0530

    aaa

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,10 +1,11 @@
 angular.module('mainCtrl', [])
 
-.controller('mainController', function() {
+.controller('mainController', [ '$scope' ,function($scope) {
 	var vm = this;	
 	vm.message = 'this is my message!';
     
     
     vm.username = '\\w+@illinois.edu';
 
-});
+    $scope.user = 'Hi user';
+}]);
