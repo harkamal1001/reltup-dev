93d4372e0524738f1b0b8d6d536f9b4e2f26cee7
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 15:06:21 +0530

    server

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ var path 	   = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
 
 app.use(express.static(__dirname + '/public'));
 
