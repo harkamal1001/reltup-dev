d8da0563192a7f0f6645634b73ef32cf8217d9c0
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 13:59:04 +0530

    validation issue resolved

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,29 +1,26 @@
 angular.module('mainCtrl', [])
 
-.controller('mainController', [ '$scope' ,function($scope) {
-	var vm = this;	
-	vm.message = 'this is my message!';
-    
-    $scope.LoginForm;
-    $scope.validUsername ='';
-    $scope.validPassword ='';
-    $scope.username = '\\w+@illinois.edu';
+    .controller('mainController', [ '$scope' ,function($scope) {
+//----------- used variables -----------------------------     
+        $scope.LoginForm;
+        $scope.validUsername ='';
+        $scope.validPassword ='';
+        $scope.username = '\\w+@illinois.edu';
+        $scope.user = 'Hi user';
+        
+//----------- validation Error -----------------------------
+        $scope.pass_err = 'Password required';
+        $scope.pass_err_length_max = 'Password is too long.';
+        $scope.pass_err_length_min = 'Username is too short.';
+        $scope.email_err = 'Email must be like user@illinois.edu';
+        
+//----------- Login form #starts -----------------------------
+        $scope.submitLoginForm = function() {
+            
+//----------- check to make sure the form is completely valid
+            if ($scope.LoginForm.$valid) {
+                alert('our form is amazing');
+            }
+        }; // Login form #ends
 
-    $scope.user = 'Hi user';
-    
-    $scope.submitLoginForm = function() {
-            // check to make sure the form is completely valid
-        if ($scope.LoginForm.$valid) {
-            alert('our form is amazing');
-        }
-        else{
-            alert('our form is not amazing');
-        }
-    };
-    
-    //validation Error
-    $scope.pass_err = 'password required';
-    $scope.pass_err_length_max = 'Password is too long.';
-    $scope.pass_err_length_min = 'Username is too short.';
-    $scope.email_err = 'Email must be like user@illinois.edu';
-}]);
+    }]); // controller #ends
