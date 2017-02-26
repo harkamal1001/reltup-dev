f377bb6328c39418a23eed5f77621415ecaa7b90
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 15:25:12 +0530

    server

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var path 	   = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// configure our app to handle CORS requests
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -21,24 +20,12 @@ app.use(function(req, res, next) {
 });
 
 
-// log all requests to the console 
-//app.use(morgan('dev'));
 
-// connect to our database 
-mongoose.connect(config.database); 
 
 
 app.use(express.static(__dirname + '/public'));
 
-// API ROUTES ------------------------
-var apiRoutes = require('./app/routes/api')(app, express);
-     //apiRoutes= require('./app/routes/api2')(app, express);
-app.use('/api', apiRoutes);
-
-// MAIN CATCHALL ROUTE --------------- 
-// SEND USERS TO FRONTEND ------------
-// has to be registered after API ROUTES
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
